e116c8960d21b80b00000a3e737b3ae22bcb37f5
HardDriver <ormo@mail.ru>
2023-02-23 20:23:27 +0300

    start

---
+++ test1_square/Program.cs
@@ -0,0 +1,7 @@
+﻿// Квадрат введенного числа
+Console.Clear();
+Console.Write("Ведите число:");
+int num Console.ReadLine();
+num = num * num;
+Console.Write("Квадрат числа = ");
+Console.WriteLine(num);
