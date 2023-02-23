6b3e1a260d10fc5c6c26f0f12f25d98015541b11
HardDriver <ormo@mail.ru>
2023-02-23 22:21:26 +0300

    вывод квадрата числа двумя способами

--- test1_square/Program.cs
+++ test1_square/Program.cs
@@ -2,6 +2,6 @@
 Console.Clear();
 Console.Write("Ведите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int itog = num * num;
+int itog = (int)Math.Pow(num, 2);
 Console.WriteLine($"Квадрат числа {num} = {itog}");
 Console.WriteLine("Квадрат числа " + num + " = " + itog);
