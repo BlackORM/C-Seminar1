f1bd87882d4340beac1a23d898668c38d40ea973
HardDriver <ormo@mail.ru>
2023-02-23 20:50:02 +0300

    вычисление квадрата числа упрощаем ввод

--- test1_square/Program.cs
+++ test1_square/Program.cs
@@ -1,9 +1,7 @@
 ﻿// Квадрат введенного числа
 Console.Clear();
 Console.Write("Ведите число: ");
-string usernum = Console.ReadLine();
-int num = Convert.ToInt32(usernum);
+int num = Convert.ToInt32(Console.ReadLine());
 int itog = num * num;
 Console.WriteLine($"Квадрат числа {num} = {itog}");
 Console.WriteLine("Квадрат числа " + num + " = " + itog);
-
